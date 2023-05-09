9c177f9e43c539fabed2ff87664dae096fffd257
Gunethra <117150479+Gunethra@users.noreply.github.com>
2023-05-09 12:37:37 +0530

    Made seperate pmf and cdf plots

---
+++ Assignment_2/code/cdf.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n=11
+pmf = np.full((n,), 1/8)
+cdf = np.cumsum(pmf)
+for i in range(8,n):
+    cdf[i] = 1
+
+fig, ax = plt.subplots()
+ax.stem(np.arange(1, n+1), cdf)
+
+ax.set_xlabel('Outcome', fontsize="45", labelpad=20)
+ax.set_ylabel('CDF', fontsize="45", labelpad=20)
+ax.set_title('Cumulative Distribution Function for the spinner', fontsize="45", pad=30)
+
+ax.set_xticks(np.arange(1, n+1))
+ax.set_xticklabels(np.arange(1, n+1))
+
+plt.xticks(fontsize=36)
+plt.yticks(fontsize=36)
+
+ax.set_ylim([0, 1.2])
+plt.show()
